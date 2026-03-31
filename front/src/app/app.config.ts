ba3e48927be53c960185b23b2055ef293921215b
Jorge Melnik <jmelnik19@gmail.com>
2026-03-31 11:20:36 -0300

    primeng

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(withEventReplay()),
+
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    MessageService,
+    ConfirmationService,
+  ],
 };
