4d742b2375d4070c1b8b9ed101828ebc9e34718f
Jorge Melnik <jmelnik19@gmail.com>
2026-04-19 21:47:03 -0300

    detalle swagger

--- api/src/plugins/swagger.ts
+++ api/src/plugins/swagger.ts
@@ -12,19 +12,19 @@ export default fp(async (fastify: FastifyInstance) => {
       info: {
         title: 'De Acá - API Documentation',
         description: `
-## Endpoints para productores y consumidores de productos orgánicos. 
+Endpoints para productores y consumidores de productos orgánicos.   
 
-## Responses
+Responses  
 
 - Recuersos que devuelven colecciones que necesitan paginación, usan el esquema DeAcaListResponse
 - Recuersos que devuelven colecciones que NO necesitan paginación, usan el esquema Type.Array(miTipo). Sin perjuicio de usar ref.
 - Recursos que devuelven objeto usan el esquema de los datos.
 
-## Requests
+Requests  
 
 - Recuersos que devuelven colecciones que necesitan paginación deben especificar queryString de forma obligatoria.
 
-## Slugs
+Slugs  
 - Entidades que tienen un atributo slug_entidad, usarán dicho slug para los GET. Para los demás métodos se seguirá usando el id_entidad.
         `,
         version: '1.0.0',
