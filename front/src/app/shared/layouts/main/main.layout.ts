10e0090298509a11d1d805d97d5649ed1a0a7ff5
Jorge Melnik <jmelnik19@gmail.com>
2026-04-28 14:21:49 +0000

    pude centrar verticalmente la card de login

--- front/src/app/shared/layouts/main/main.layout.ts
+++ front/src/app/shared/layouts/main/main.layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Button } from 'primeng/button';
+import { TabsModule, Tabs, TabList, Tab, TabPanel, TabPanels } from 'primeng/tabs';
 
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button, TabsModule, Tabs, TabList, Tab, TabPanel, TabPanels],
   templateUrl: './main.layout.html',
   styleUrl: './main.layout.css',
 })
