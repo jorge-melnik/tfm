78449de328127f39240a53b8332fb6a5764a3b21
Jorge Melnik <jmelnik19@gmail.com>
2026-05-08 14:19:23 +0000

    sintaxis

--- db/init/04-categorias.sql
+++ db/init/04-categorias.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS categorias (
     ),-- TODO: TRIGGER para asegurarse que no se cambia el slug_categoria
     descripcion TEXT,
     icono VARCHAR(24), --FIXME: Cambiar a NOT NULL
-    color VARCHAR(7) NOT NULL DEFAULT '#6366F1'; -- Color hexadecimal
+    color VARCHAR(7) NOT NULL DEFAULT '#6366F1', -- Color hexadecimal
     fecha_creacion TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizacion TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fecha_eliminacion TIMESTAMP WITH TIME ZONE,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS etiquetas (
         AND slug_etiqueta ~ '^[a-zA-Z0-9-]+$' 
     ),-- TODO: TRIGGER para asegurarse que no se cambia el slug_etiqueta
     imagen TEXT, --FIXME: Podría ser clase icono o url de imagen. --FIXME: Hacer not null
-    color VARCHAR(7) NOT NULL DEFAULT '#6366F1'; -- Color hexadecimal FIME: Sacar el default
+    color VARCHAR(7) NOT NULL DEFAULT '#6366F1' -- Color hexadecimal FIME: Sacar el default
 );
 
 CREATE TABLE IF NOT EXISTS subcategoria_etiquetas (
