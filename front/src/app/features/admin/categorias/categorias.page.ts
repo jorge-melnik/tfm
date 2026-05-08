21f0d694b5d604572e6b4ae84d5a4a80fe3588eb
Jorge Melnik <jmelnik19@gmail.com>
2026-05-08 15:12:00 +0000

    tabla categorias

--- front/src/app/features/admin/categorias/categorias.page.ts
+++ front/src/app/features/admin/categorias/categorias.page.ts
@@ -1,17 +1,54 @@
-import { Component, computed, inject, resource } from '@angular/core';
+import { Component, computed, inject, resource, signal } from '@angular/core';
 import { CategoriasService } from '@shared/services/categorias.service';
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { AdminTable } from '@shared/components/admin-table/admin.table';
+import { Button } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+import { InputText } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { InputIcon } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { CommonModule, DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-categorias',
-  imports: [TableModule, ProgressSpinnerModule, AdminTable],
+  imports: [
+    TableModule,
+    ProgressSpinnerModule,
+    TitleCasePipe,
+    DatePipe,
+    TableModule,
+    CommonModule,
+    IconFieldModule,
+    InputIcon,
+    FormsModule,
+    InputText,
+    Button,
+    RouterLink,
+  ],
   templateUrl: './categorias.page.html',
   styleUrl: './categorias.page.css',
 })
 export class CategoriasPage {
   private readonly _categoriasService = inject(CategoriasService);
+
+  public excludeColumns: string[] = [];
+
+  public searchValue = signal<string | null>(null);
+
+  columns = computed(() => {
+    const data = this.categoriasResource.value();
+    const firstItem = data[0];
+    if (!firstItem) return [];
+
+    return Object.keys(firstItem)
+      .filter((key) => !this.excludeColumns.includes(key)) //La key no está en las excluidas
+      .map((key) => ({
+        key,
+        keyTitle: this.formatHeader(key),
+      }));
+  });
 
   public categoriasResource = resource({
     defaultValue: [],
@@ -24,4 +61,15 @@ export class CategoriasPage {
       return [];
     },
   });
+
+  private formatHeader(key: string): string {
+    if (key.startsWith('id_')) return '#';
+    if (key.startsWith('slug_')) return 'Slug';
+    return key.replace(/_/g, ' '); ///reemplazamos guion bajo por espacio
+  }
+
+  clear(dt: Table) {
+    this.searchValue.set(null);
+    dt.reset();
+  }
 }
