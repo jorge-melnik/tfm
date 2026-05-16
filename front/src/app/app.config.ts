72a955264da85cf63c2b2c17ca24fbb0b83dfc20
Jorge Melnik <jmelnik19@gmail.com>
2026-05-16 19:06:36 +0000

    sin ssr

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
-    provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
         preset: Aura,
