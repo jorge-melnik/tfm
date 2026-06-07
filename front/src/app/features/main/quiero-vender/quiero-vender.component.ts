3dc7fdec057bca5cb691fd4105f200dda21a3bd2
Jorge Melnik <jmelnik19@gmail.com>
2026-06-07 22:49:59 +0000

    empezando  quiero vender

--- front/src/app/features/main/quiero-vender/quiero-vender.component.ts
+++ front/src/app/features/main/quiero-vender/quiero-vender.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { TextareaModule } from 'primeng/textarea';
+import { Card } from 'primeng/card';
+import { FloatLabel } from 'primeng/floatlabel';
+import { Button } from 'primeng/button';
+
+import { IftaLabelModule, IftaLabel } from 'primeng/iftalabel';
 
 @Component({
   selector: 'app-quiero-vender',
-  imports: [],
+  imports: [TextareaModule, Card, Button, IftaLabel],
   templateUrl: './quiero-vender.component.html',
   styleUrl: './quiero-vender.component.css',
 })
