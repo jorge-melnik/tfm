20161298280bacc2d7adf2003a4a0d03564e0435
Jorge Melnik <jmelnik19@gmail.com>
2026-07-26 18:45:22 +0000

    parece que el filtro anda

--- front/src/app/features/productor/productos/components/productos/productos.table.ts
+++ front/src/app/features/productor/productos/components/productos/productos.table.ts
@@ -1,29 +1,19 @@
-import { Component, computed, inject, resource, signal } from '@angular/core';
-
+import { Component, inject, input, model, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
-
 import { TrashIcon, PencilIcon, CheckIcon, TimesIcon, BanIcon } from 'primeng/icons';
 import { SelectModule } from 'primeng/select';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTagsModule } from 'primeng/inputtags';
 import { InputTextModule } from 'primeng/inputtext';
 import { Producto } from '@shared/types/producto';
-import { Categoria, Subcategoria } from '@shared/types/categoria';
 import { environment } from '@env/environment';
-import { UserStore } from '@shared/services/stores/user.store';
-import { EtiquetasService } from '@shared/services/etiquetas.service';
-import { SubcategoriasService } from '@shared/services/subcategorias.service';
-import { CategoriasService } from '@shared/services/categorias.service';
-import { ProductosService } from '@shared/services/productos.service';
-import { Checkbox } from 'primeng/checkbox';
 import { FotoCarrusel } from '@shared/components/foto-carrusel/foto.carrusel';
-import { PreferenciasStore } from '@shared/services/stores/preferencias.store';
 import { SortOption } from '@shared/types/util';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
-import { ApiQueryParams } from '@shared/types/api.types';
+import { PreferenciasStore } from '@shared/services/stores/preferencias.store';
+import { Categoria, Subcategoria } from '@shared/types/categoria';
+import { Etiqueta } from '@shared/types/etiqueta';
 
 @Component({
   selector: 'app-productos-table',
@@ -46,91 +36,18 @@ import { ApiQueryParams } from '@shared/types/api.types';
   styleUrl: './productos.table.css',
 })
 export class ProductosTable {
-  private readonly _productosService = inject(ProductosService);
-  private readonly _categoriaService = inject(CategoriasService);
-  private readonly _subcategoriaService = inject(SubcategoriasService);
-  private readonly _etiquetaService = inject(EtiquetasService);
-  private readonly _userStore = inject(UserStore);
-  private readonly _preferenciasStore = inject(PreferenciasStore);
-
-  private readonly _router = inject(Router);
-  private readonly _route = inject(ActivatedRoute);
-
+  private _preferenciasStore = inject(PreferenciasStore);
   public cdnUrl = environment.cdnUrl;
 
-  public page = signal(1);
-  public limit = this._preferenciasStore.limit;
-  public first = computed(() => (this.page() - 1) * this.limit());
-  public sortKey = signal<string>('');
-  public sortOrder = signal<number>(0);
-  public sortField = signal<string>('');
-  public sortOptions!: SortOption[];
+  public total = input.required<number>();
+  public page = model.required<number>();
+  public limit = model.required<number>();
+  public first = input.required<number>();
 
-  private _queryParams = toSignal(this._route.queryParamMap, {
-    initialValue: convertToParamMap({}),
-  });
-
-  private _pathParams = toSignal(this._route.paramMap, {
-    initialValue: convertToParamMap({}),
-  });
-
-  // public productosResource = resource({
-  //   defaultValue: [] as Producto[],
-  //   loader: async () => {
-  //     const user = this._userStore.user();
-  //     if (!user) throw new Error('Usuario no autenticado');
-  //     const productos = (
-  //       await this._productosService.getBy({
-  //         queryParams: {
-  //           id_productor: user.id_usuario,
-  //         },
-  //       })
-  //     ).data;
-  //     return productos;
-  //   },
-  // });
-
-  public productosResource = resource({
-    params: () => ({
-      limit: this.limit(),
-      page: this.page(),
-      sort: this.sortField(),
-      sort_direction: this.sortOrder() === -1 ? 'DESC' : 'ASC',
-    }),
-    loader: async ({ params }) => {
-      const user = this._userStore.user();
-      if (!user) throw new Error('Usuario no autenticado');
-      const { limit, page, sort, sort_direction } = params;
-      const queryParams: ApiQueryParams = {
-        limit,
-        page,
-        sort,
-        sort_direction,
-        id_productor: user.id_usuario,
-      };
-      return this._productosService.getBy({
-        queryParams,
-      });
-    },
-  });
-
-  public categoriasResource = resource({
-    defaultValue: [] as Categoria[],
-    loader: () => this._categoriaService.getAll(),
-  });
-
-  public subcategoriasResource = resource({
-    defaultValue: [] as Subcategoria[],
-    loader: () => this._subcategoriaService.getAll(),
-  });
-
-  public etiquetasResource = resource({
-    defaultValue: [],
-    loader: async () => {
-      const etiquetas = await this._etiquetaService.getAll();
-      return etiquetas;
-    },
-  });
+  public productos = input.required<Producto[]>();
+  public categorias = input.required<Categoria[]>();
+  public subcategorias = input.required<Subcategoria[]>();
+  public etiquetas = input.required<Etiqueta[]>();
 
   public productoSeleccionado = signal<Producto | null>(null);
 
--- front/src/app/features/productor/productos/productos.page.ts
+++ front/src/app/features/productor/productos/productos.page.ts
@@ -1,11 +1,161 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, model, OnInit, resource, signal } from '@angular/core';
 import { ProductosTable } from './components/productos/productos.table';
+import { ProductosFilter } from '@shared/components/productos-filter/productos.filter';
+import { ApiQueryParams } from '@shared/types/api.types';
+import { environment } from '@env/environment';
+import { Categoria, Subcategoria } from '@shared/types/categoria';
+import { ProductosService } from '@shared/services/productos.service';
+import { CategoriasService } from '@shared/services/categorias.service';
+import { SubcategoriasService } from '@shared/services/subcategorias.service';
+import { EtiquetasService } from '@shared/services/etiquetas.service';
+import { PreferenciasStore } from '@shared/services/stores/preferencias.store';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarritoService } from '@shared/services/carrito.service';
+import { UserStore } from '@shared/services/stores/user.store';
+import { DialogService } from '@shared/services/dialog.service';
+import { Etiqueta } from '@shared/types/etiqueta';
 
 @Component({
   selector: 'app-productoos',
-  imports: [ProductosTable],
+  imports: [ProductosTable, ProductosFilter],
   templateUrl: './productos.page.html',
 
   styleUrl: './productos.page.css',
 })
-export class ProductosPage {}
+export class ProductosPage implements OnInit {
+  private readonly _productoService = inject(ProductosService);
+  private readonly _categoriaService = inject(CategoriasService);
+  private readonly _subcategoriaService = inject(SubcategoriasService);
+  private readonly _etiquetasService = inject(EtiquetasService);
+  private readonly _preferenciasStore = inject(PreferenciasStore);
+  private readonly _router = inject(Router);
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _carritoService = inject(CarritoService);
+  private readonly _userStore = inject(UserStore);
+  private readonly _dialogService = inject(DialogService);
+
+  public cdnUrl = environment.cdnUrl;
+
+  //Signals para filtros.
+  public busqueda = model<string>('');
+  public categoria = model<string | undefined>(undefined);
+  public subcategoria = model<string | undefined>(undefined);
+  public etiquetas = signal<string[]>([]);
+  public page = signal<number>(1);
+  public limit = model<number>(this._preferenciasStore.limit());
+  public first = computed(() => ((this.page() || 1) - 1) * this.limit());
+  public sortKey = model<string>('');
+  public sortOrder = model<number>(0);
+  public sortField = model<string>('');
+
+  public categoriasResource = resource({
+    defaultValue: [] as Categoria[],
+    loader: async () => {
+      try {
+        return this._categoriaService.getAll();
+      } catch (error: any) {
+        this._dialogService.addError(error.message);
+        return [] as Categoria[];
+      }
+    },
+  });
+
+  public subcategoriasResource = resource({
+    defaultValue: [] as Subcategoria[],
+    params: () => ({ categoria: this.categoria() }),
+    loader: async ({ params }) => {
+      try {
+        const { categoria } = params;
+        if (!categoria) return this._subcategoriaService.getAll();
+        return this._categoriaService.getSubcategorias(categoria);
+      } catch (error: any) {
+        this._dialogService.addError(error.message);
+        return [] as Subcategoria[];
+      }
+    },
+  });
+
+  public etiquetasResource = resource({
+    defaultValue: [] as Etiqueta[],
+    params: () => ({
+      categoria: this.categoria(),
+      subcategoria: this.subcategoria(),
+    }),
+    loader: async ({ params }) => {
+      try {
+        const { categoria, subcategoria } = params;
+
+        if (subcategoria) return this._subcategoriaService.getEtiquetas(subcategoria);
+        if (categoria) return this._categoriaService.getEtiquetas(categoria);
+
+        //No hay ninguno de los slug
+        return this._etiquetasService.getAll();
+      } catch (error: any) {
+        this._dialogService.addError(error.message);
+        return [] as Etiqueta[];
+      }
+    },
+  });
+
+  public totalProductos = computed<number>(() => {
+    const productos = this.productosResource.value()?.data || [];
+    return productos.length;
+  });
+  public productosResource = resource({
+    params: () => ({
+      categoria: this.categoria(),
+      subcategoria: this.subcategoria(),
+      etiquetas: this.etiquetas(),
+      limit: this.limit(),
+      page: this.page(),
+      sort: this.sortField(),
+      sort_direction: this.sortOrder() === -1 ? 'DESC' : 'ASC',
+      busqueda: this.busqueda(),
+      user: this._userStore.user(),
+    }),
+    loader: async ({ params }) => {
+      const {
+        user,
+        categoria,
+        subcategoria,
+        etiquetas,
+        limit,
+        page,
+        sort,
+        sort_direction,
+        busqueda,
+      } = params;
+
+      if (!user) throw new Error('Usuario no autenticado');
+
+      const queryParams: ApiQueryParams = { id_productor: user.id_usuario };
+      const pagination: ApiQueryParams = { limit, page, sort, sort_direction };
+      // if (limit) pagination['limit'] = limit;
+      // if (page) pagination['page'] = page;
+      // if (sort) pagination['sort'] = sort;
+      // if (sort_direction) pagination['sort_direction'] = sort_direction;
+
+      if (categoria) queryParams['categoria'] = categoria;
+      if (subcategoria) queryParams['subcategoria'] = subcategoria;
+      if (etiquetas) queryParams['etiquetas'] = etiquetas;
+      if (busqueda) queryParams['busqueda'] = busqueda;
+
+      try {
+        const response = await this._productoService.getBy({ queryParams, pagination });
+
+        console.log({ response });
+
+        return response;
+      } catch (error: any) {
+        this._dialogService.addError(error.message);
+        return { data: [], meta: { total: 0 } };
+      }
+    },
+  });
+
+  public layout = signal<'grid' | 'list'>('grid'); // Estado del diseño (tarjeta o lista)
+
+  ngOnInit(): void {
+    if (!this.limit()) this.limit.set(this._preferenciasStore.limit());
+  }
+}
