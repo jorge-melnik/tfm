9c4ae1f5f48f794181cf6ffece650d2d7c7de5f3
Jorge Melnik <jmelnik19@gmail.com>
2026-08-17 22:28:13 +0000

    menu usuario ok

--- front/src/app/shared/components/top-bar/top-bar.component.ts
+++ front/src/app/shared/components/top-bar/top-bar.component.ts
@@ -1,53 +1,31 @@
 import { Component, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { MenuItem } from 'primeng/api';
 import { MenuModule } from 'primeng/menu';
 import { ButtonDirective } from 'primeng/button';
 import { UserStore } from '@shared/services/stores/user.store';
+import { Popover, PopoverModule } from 'primeng/popover';
+import { Cog, Shop, ShoppingCart, SignIn, SignOut, User, UserEdit } from '@primeicons/angular';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [RouterLink, MenuModule, ButtonDirective],
+  imports: [
+    RouterLink,
+    MenuModule,
+    ButtonDirective,
+    Popover,
+    PopoverModule,
+    UserEdit,
+    User,
+    Shop,
+    Cog,
+    Shop,
+    ShoppingCart,
+    SignOut,
+    SignIn,
+  ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css',
 })
 export class TopBarComponent {
-  private _userStore = inject(UserStore);
-
-  public items = computed<MenuItem[]>(() => {
-    const user = this._userStore.user();
-    if (!user)
-      return [
-        {
-          label: 'No autenticado',
-          items: [{ label: 'Iniciar sesión', icon: 'pi pi-user-edit', routerLink: '/auth/login' }],
-        },
-      ];
-
-    const items = [{ label: 'Mis datos', icon: 'pi pi-user-edit', routerLink: '/' }];
-    //Funciones con roles actuales
-    if (this._userStore.userHasRole('CONSUMIDOR') && user.rol_actual !== 'CONSUMIDOR')
-      items.push({ label: 'Comprar', icon: 'pi pi-shopping-cart', routerLink: '/consumidor' });
-    if (this._userStore.userHasRole('PRODUCTOR') && user.rol_actual !== 'PRODUCTOR')
-      items.push({ label: 'Vender', icon: 'pi pi-shop', routerLink: '/productor' });
-    if (this._userStore.userHasRole('ADMIN') && user.rol_actual !== 'ADMIN')
-      items.push({ label: 'Administrar', icon: 'pi pi-cog', routerLink: '/admin' });
-
-    //Quiero tal (activar otro rol)
-    if (!this._userStore.userHasRole('CONSUMIDOR'))
-      items.push({
-        label: 'Quiero Comprar',
-        icon: 'pi pi-shopping-cart',
-        routerLink: '/quiero/comprar',
-      });
-    if (!this._userStore.userHasRole('PRODUCTOR'))
-      items.push({
-        label: 'Quiero Vender',
-        icon: 'pi pi-shop',
-        routerLink: '/quiero/vender',
-      });
-
-    items.push({ label: 'Logout', icon: 'pi pi-sign-out', routerLink: '/auth/logout' });
-    return [{ label: user.username, items }];
-  });
+  public readonly userStore = inject(UserStore);
 }
