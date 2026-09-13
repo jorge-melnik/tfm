da407342a403b2b8fd11806f302ed056d495f380
Jorge Melnik <jmelnik19@gmail.com>
2026-09-13 16:50:58 +0000

    ubicación de envío al confirmar compra

--- front/src/app/features/consumidor/carrito/carrito.page.ts
+++ front/src/app/features/consumidor/carrito/carrito.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, resource, signal } from '@angular/core';
 import { CarritoService } from '@shared/services/carrito.service';
 import { PreferenciasStore } from '@shared/services/stores/preferencias.store';
 import { DataView } from 'primeng/dataview';
@@ -12,10 +12,32 @@ import { ComprasService } from '@shared/services/compras.service';
 import { UserStore } from '@shared/services/stores/user.store';
 import { CompraPOST } from '@shared/types/compra';
 import { Router } from '@angular/router';
+import { UsuariosService } from '@shared/services/usuarios.service.ts';
+import { Select } from 'primeng/select';
+import { Ubicacion } from '@shared/types/ubicacion';
+import { FormsModule } from '@angular/forms';
+import { Check, Phone } from '@primeicons/angular';
+import { FloatLabel } from 'primeng/floatlabel';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-carrito',
-  imports: [DataView, ItemCarritoComponent, Tag, ButtonDirective],
+  imports: [
+    DataView,
+    ItemCarritoComponent,
+    Tag,
+    ButtonDirective,
+    Select,
+    FormsModule,
+    Check,
+    FloatLabel,
+    IconField,
+    InputIcon,
+    Phone,
+    InputTextModule,
+  ],
   templateUrl: './carrito.page.html',
 
   styleUrl: './carrito.page.css',
@@ -25,6 +47,7 @@ export class CarritoPage implements OnInit {
     this._carritoService.recargarCarrito();
   }
   private readonly _router = inject(Router);
+  private readonly usuariosService = inject(UsuariosService);
   private _carritoService = inject(CarritoService);
   public readonly _preferenciasStore = inject(PreferenciasStore);
   public readonly userStore = inject(UserStore);
@@ -36,8 +59,23 @@ export class CarritoPage implements OnInit {
 
   protected totalCarrito = this._carritoService.totalCarrito;
 
-  public direccion_envio = signal<string>('');
   public contacto_receptor = signal<string>('');
+
+  public ubicacionesResource = resource({
+    params: () => {
+      const username = this.userStore.user()?.username;
+      if (!username) return undefined;
+      return { username };
+    },
+    loader: async ({ params }) => {
+      const { username } = params;
+      return this.usuariosService.getUbicaciones(username);
+    },
+  });
+
+  public ubicaciones = computed(() => this.ubicacionesResource.value() ?? []);
+
+  public ubicacionSeleccionada = signal<Ubicacion | null>(null);
 
   public async onBorrar(item: ItemCarrito) {
     try {
@@ -60,15 +98,20 @@ export class CarritoPage implements OnInit {
   public async confirmarCompra() {
     try {
       const username = this.userStore.user()?.username;
-      if (!username) return;
+      const ubicacion = this.ubicacionSeleccionada();
+      const contacto = this.contacto_receptor();
+
+      if (!username || !ubicacion || !contacto) return;
       const datos: CompraPOST = {
-        direccion_envio: this.direccion_envio(),
-        contacto_receptor: this.contacto_receptor(),
+        direccion_envio: `${ubicacion.direccion} (${(ubicacion.localidad, ubicacion.departamento)})`,
+        contacto_receptor: contacto,
       };
 
       //TODO: Falta agregar direccion envio y contacto en el formulario
       const compra = await this._comprasService.create(datos, { username });
       this._carritoService.recargarCarrito();
+      this.contacto_receptor.set('');
+      this.ubicacionSeleccionada.set(null);
       this._router.navigate(['consumidor', 'compras', compra.id_compra]);
     } catch (error: any) {
       const mensaje = error.error ? error.error.message : error.message;
