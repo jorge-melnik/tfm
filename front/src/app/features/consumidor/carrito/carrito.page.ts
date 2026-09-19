68c51d381ac0b7144a7ce7b291102a4c4a3cf814
Jorge Melnik <jmelnik19@gmail.com>
2026-09-19 03:56:57 +0000

    validacion con confirmar compra

--- front/src/app/features/consumidor/carrito/carrito.page.ts
+++ front/src/app/features/consumidor/carrito/carrito.page.ts
@@ -7,20 +7,26 @@ import { environment } from '@env/environment';
 import { Tag } from 'primeng/tag';
 import { ItemCarrito } from '@shared/types/item-carrito';
 import { DialogService } from '@shared/services/dialog.service';
-import { ButtonDirective } from 'primeng/button';
 import { ComprasService } from '@shared/services/compras.service';
 import { UserStore } from '@shared/services/stores/user.store';
 import { CompraPOST } from '@shared/types/compra';
 import { Router } from '@angular/router';
 import { UsuariosService } from '@shared/services/usuarios.service.ts';
 import { Select } from 'primeng/select';
-import { Ubicacion } from '@shared/types/ubicacion';
 import { FormsModule } from '@angular/forms';
 import { Check, Phone } from '@primeicons/angular';
 import { FloatLabel } from 'primeng/floatlabel';
 import { IconField } from 'primeng/iconfield';
 import { InputIcon } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
+import { form, required, FormField } from '@angular/forms/signals';
+import { Ubicacion } from '@shared/types/ubicacion';
+import { ButtonModule } from 'primeng/button';
+
+type DatosEnvio = {
+  contacto_receptor: string;
+  id_ubicacion: number | null;
+};
 
 @Component({
   selector: 'app-carrito',
@@ -28,7 +34,7 @@ import { InputTextModule } from 'primeng/inputtext';
     DataView,
     ItemCarritoComponent,
     Tag,
-    ButtonDirective,
+    ButtonModule,
     Select,
     FormsModule,
     Check,
@@ -37,15 +43,13 @@ import { InputTextModule } from 'primeng/inputtext';
     InputIcon,
     Phone,
     InputTextModule,
+    FormField,
   ],
   templateUrl: './carrito.page.html',
 
   styleUrl: './carrito.page.css',
 })
 export class CarritoPage implements OnInit {
-  async ngOnInit(): Promise<void> {
-    this._carritoService.recargarCarrito();
-  }
   private readonly _router = inject(Router);
   private readonly usuariosService = inject(UsuariosService);
   private _carritoService = inject(CarritoService);
@@ -59,23 +63,36 @@ export class CarritoPage implements OnInit {
 
   protected totalCarrito = this._carritoService.totalCarrito;
 
-  public contacto_receptor = signal<string>('');
+  public datosEnvio = signal<DatosEnvio>({ contacto_receptor: '', id_ubicacion: null });
 
-  public ubicacionesResource = resource({
-    params: () => {
-      const username = this.userStore.user()?.username;
-      if (!username) return undefined;
-      return { username };
-    },
-    loader: async ({ params }) => {
-      const { username } = params;
-      return this.usuariosService.getUbicaciones(username);
-    },
+  public ubicacionSeleccionada = computed(() => {
+    const id_ubicacion: number | null = this.datosEnvio().id_ubicacion;
+    const ubicaciones: Ubicacion[] | undefined = this.ubicaciones();
+    if (!id_ubicacion || !ubicaciones) return;
+    return ubicaciones.find((u) => u.id_ubicacion === id_ubicacion);
   });
 
-  public ubicaciones = computed(() => this.ubicacionesResource.value() ?? []);
+  public datosEnvioForm = form(this.datosEnvio, (path) => {
+    required(path.contacto_receptor, {
+      message: 'Por favor completa el contacto receptor',
+    });
 
-  public ubicacionSeleccionada = signal<Ubicacion | null>(null);
+    required(path.id_ubicacion, {
+      message: 'Ingresa donde quieres recibir el pedido.',
+    });
+  });
+
+  public ubicaciones = signal<Ubicacion[]>([]);
+
+  async ngOnInit(): Promise<void> {
+    this._carritoService.recargarCarrito();
+    const username = this.userStore.user()?.username;
+    if (!username) return;
+    const ubicaciones = await this.usuariosService.getUbicaciones(username);
+    this.ubicaciones.set(ubicaciones);
+    const id_ubicacion = ubicaciones.length > 0 ? ubicaciones[0].id_ubicacion : null;
+    this.datosEnvio.set({ ...this.datosEnvio(), id_ubicacion });
+  }
 
   public async onBorrar(item: ItemCarrito) {
     try {
@@ -95,22 +112,32 @@ export class CarritoPage implements OnInit {
     }
   }
 
-  public async confirmarCompra() {
+  public async confirmarCompra(event: Event) {
+    event.preventDefault();
+    if (!this.datosEnvioForm().valid()) {
+      this.datosEnvioForm().markAsTouched();
+      this._dialogService.addWarn('Por favor, completa los datos para el envío.');
+      return;
+    }
     try {
       const username = this.userStore.user()?.username;
       const ubicacion = this.ubicacionSeleccionada();
-      const contacto = this.contacto_receptor();
+      if (!ubicacion) {
+        this._dialogService.addWarn('Selecciona la ubicación de envío.');
+        return;
+      }
+      const contacto = this.datosEnvio().contacto_receptor;
+      const direccionEnvio = `${ubicacion.direccion} (${(ubicacion.localidad, ubicacion.departamento)})`;
 
-      if (!username || !ubicacion || !contacto) return;
+      if (!username || !ubicacion || !contacto || !direccionEnvio) return;
       const datos: CompraPOST = {
-        direccion_envio: `${ubicacion.direccion} (${(ubicacion.localidad, ubicacion.departamento)})`,
+        direccion_envio: direccionEnvio,
         contacto_receptor: contacto,
       };
 
       const compra = await this._comprasService.create(datos, { username });
       this._carritoService.recargarCarrito();
-      this.contacto_receptor.set('');
-      this.ubicacionSeleccionada.set(null);
+      this.datosEnvio.set({ contacto_receptor: '', id_ubicacion: null });
       this._router.navigate(['consumidor', 'compras', compra.id_compra]);
     } catch (error: any) {
       const mensaje = error.error ? error.error.message : error.message;
